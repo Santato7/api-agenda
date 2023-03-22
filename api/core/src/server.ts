555e495f6500e1a7211f146abffc54c4abe0c567
santato <santatolucas7@gmail.com>
2023-03-22 19:35:48 -0300

    modified:   api/core/src/server.ts

--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -2,31 +2,58 @@ import express from "express";
 import "dotenv/config";
 const mysql = require("mysql2/promise");
 
+var app = express();
+
+if (!module.parent) {
+  app.listen(3000);
+  console.log("Express started on port 3000");
+}
+
 const config = {
   host: process.env.DATABASE_HOST,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
 };
 
-var app = express();
+app.get("/compromisso", async function (req: any, res: any) {
+  const idCompromisso: number = req.query.id;
 
-app.get("/", async function (req: any, res: any) {
+  let compromissos: string = "[]";
+
+  if (idCompromisso != null) {
+    compromissos = await getCompromissoById(idCompromisso);
+  } else {
+    compromissos = await getAllCompromissos();
+  }
+
+  res.json(compromissos);
+});
+
+const getAllCompromissos = async () => {
   const connection = await mysql.createConnection(process.env.DATABASE_URL);
 
   try {
     const [rows, fields] = await connection.execute(
       "SELECT * FROM `COMPROMISSO`"
     );
-
-    res.send(rows);
+    return rows;
   } finally {
     connection.end();
   }
-});
+};
 
-if (!module.parent) {
-  app.listen(3000);
-  console.log("Express started on port 3000");
-}
+const getCompromissoById = async (_idCompromisso: number) => {
+  const connection = await mysql.createConnection(process.env.DATABASE_URL);
+
+  try {
+    const [rows, fields] = await connection.execute(
+      "SELECT * FROM `COMPROMISSO` WHERE `ID_COMPROMISSO` = ?",
+      [_idCompromisso]
+    );
+    return rows;
+  } finally {
+    connection.end();
+  }
+};
 
 export default app;
